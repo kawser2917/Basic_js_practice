987703a360b363b454674696a558ab61732b7c20
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-20 23:50:31 +0600

    ToLowerCase Condition

---
+++ case.js
@@ -0,0 +1,10 @@
+let userName = "Kawser Ahmed";
+let inputName = "kawser Ahmed";
+
+if(userName.toLowerCase() === inputName.toLowerCase()){
+    console.log("Valid User")
+}
+else{
+    console.log("Invalid User")
+}
+// Use can use toUpperCase Also for matching this kind of logic
