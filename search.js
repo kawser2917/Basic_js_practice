5815621841170057ca439ad9a20bf717804617a5
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-21 00:37:45 +0600

    string searching starswith understanding

---
+++ search.js
@@ -0,0 +1,17 @@
+const lyrics = "Sada Sada kala kala rong jomeche sada kala"
+
+let searchString  = "sAda"
+let searchStringLower = searchString.toLowerCase()
+let stringLower = lyrics.toLowerCase()
+
+let doesExist = stringLower.includes(searchStringLower)
+let doesExistInOneLine = lyrics.toLowerCase().includes(searchString.toLowerCase())
+console.log(doesExist)
+console.log(doesExistInOneLine)
+
+let positionOfString = lyrics.indexOf("rong")
+console.log(positionOfString)
+
+let beginingCheck = lyrics.toLowerCase().startsWith("sada")
+
+console.log(beginingCheck)
